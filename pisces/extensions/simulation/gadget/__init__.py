84c6a0563d572e60a5de5e6d35df74d86ff84152
Eliza Diggins <eliza.diggins@utah.edu>
2025-08-25 13:44:33 -0700

    feat: reimplemented Gadget support.

--- pisces/extensions/simulation/gadget/__init__.py
+++ pisces/extensions/simulation/gadget/__init__.py
@@ -9,7 +9,7 @@ for Gadget-based simulations, including Gadget-2 and Gadget-3.
 For more information on Gadget, see the `official documentation <https://wwwmpa.mpa-garching.mpg.de/gadget/>`_.
 """
 
-__all__ = ["Gadget2Frontend"]
+__all__ = ["Gadget4Frontend"]
 
 # Provide access to the frontend.
-from .frontends import Gadget2Frontend
+from .frontends import Gadget4Frontend
